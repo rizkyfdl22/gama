10d001f1dca2817207b9c0329249c015fa24ed8b
rzkyfdlla22 <rizkyfadilla70@gmail.com>
2026-04-15 10:29:40 +0700

    'login'

---
+++ app/login/page.js
@@ -0,0 +1,62 @@
+"use client";
+
+import { useState } from "react";
+import { supabase } from "../lib/supabase";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert(error.message);
+      return;
+    }
+
+    // ambil role user
+    const { data: profile } = await supabase
+      .from("profiles")
+      .select("role")
+      .eq("id", data.user.id)
+      .single();
+
+    if (profile.role === "admin") {
+      router.push("/admin");
+    } else {
+      router.push("/dashboard");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleLogin} className="login-box">
+        <h2>Login</h2>
+
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
