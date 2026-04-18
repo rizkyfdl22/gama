99d6c2ec946b1171dd12fdd1f7c70cfcfe3827f6
rzkyfdlla22 <rizkyfadilla70@gmail.com>
2026-04-18 08:18:05 +0700

    'fix-matches'

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
