605204f2d256219fec2dcfa55331d252da14cd53
rzkyfdlla22 <rizkyfadilla70@gmail.com>
2026-05-22 14:37:00 +0700

    'sitemap'

---
+++ app/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://semestaesports.id",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://semestaesports.id/blogs",
+      lastModified: new Date(),
+    },
+  ];
+}
