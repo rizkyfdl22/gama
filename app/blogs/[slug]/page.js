499e4b202bc71187b37fb7257ea6552a06a78af5
rzkyfdlla22 <rizkyfadilla70@gmail.com>
2026-05-22 13:47:59 +0700

    'latest-blogs2'

--- app/blogs/[slug]/page.js
+++ app/blogs/[slug]/page.js
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { supabase } from "@/app/lib/supabase";
 import { useParams } from "next/navigation";
+import Link from "next/link";
 import styles from "./BlogDetail.module.css";
 
 export default function BlogDetail() {
@@ -45,6 +46,44 @@ export default function BlogDetail() {
           dangerouslySetInnerHTML={{ __html: blog.content }}
         />
       </div>
+      {/* ========================= */}
+      {/* BLOGS */}
+      {/* ========================= */}
+      <section className="latest-blogs fade-up">
+
+        <div className="blog-header">
+          <h2 className="gradient-text">Latest News</h2>
+
+          <div className="blog-nav">
+            <button onClick={scrollLeft}>‹</button>
+            <button onClick={scrollRight}>›</button>
+          </div>
+        </div>
+
+        <div className="blog-slider" ref={sliderRef}>
+          {blogs.map((blog) => (
+            <Link
+              href={`/blogs/${blog.slug}`}
+              className="blog-card"
+              key={blog.id}
+            >
+              <img src={blog.thumbnail} alt={blog.title} />
+
+              <div className="blog-content">
+                <h3>{blog.title}</h3>
+
+                <p>
+                  {blog.description?.slice(0, 100)}...
+                </p>
+              </div>
+            </Link>
+          ))}
+        </div>
+
+      </section>
+
+
     </div>
+    
   );
 }
