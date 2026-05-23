d893703ae573cf9d8a00fbc9e1eed7e96c429bcf
rzkyfdlla22 <rizkyfadilla70@gmail.com>
2026-05-24 01:54:34 +0700

    revisi13

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,4 +1,4 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createBrowserClient } from "@supabase/supabase-js";
 
 export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
