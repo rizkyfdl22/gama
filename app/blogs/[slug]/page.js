33ac6b5369f8a0ef23d25f6cfb9fc6a60588e664
rzkyfdlla22 <rizkyfadilla70@gmail.com>
2026-05-24 02:33:57 +0700

    revisi18

--- app/blogs/[slug]/page.js
+++ app/blogs/[slug]/page.js
@@ -4,7 +4,9 @@ import BlogDetail from "./BlogDetail";
 export async function generateMetadata({ params }) {
   const { slug } = params;
 
-  const { data: blog } = await supabaseServer
+  const supabase = supabaseServer();
+
+  const { data: blog } = await supabase
     .from("blogs")
     .select("*")
     .eq("slug", slug)
@@ -18,60 +20,31 @@ export async function generateMetadata({ params }) {
 
   return {
     title: blog.title,
-
     description:
       blog.description ||
       "Artikel terbaru esports dari Semesta Esports.",
-
-    keywords: [
-      "esports",
-      "tournament",
-      "berita esports",
-      "portal berita",
-      "news esports",
-      "esports indonesia",
-      "gaming",
-      "semesta esports",
-      "mobile legends",
-      blog.title,
-    ],
-
-    openGraph: {
-      title: blog.title,
-      description: blog.description,
-      images: [
-        {
-          url: blog.thumbnail,
-          width: 1200,
-          height: 630,
-        },
-      ],
-      type: "article",
-    },
-
-    twitter: {
-      card: "summary_large_image",
-      title: blog.title,
-      description: blog.description,
-      images: [blog.thumbnail],
-    },
   };
 }
 
 export default async function Page({ params }) {
   const { slug } = params;
 
-  const { data: blog, error } = await supabaseServer
+  const supabase = supabaseServer();
+
+  const { data: blog, error } = await supabase
     .from("blogs")
     .select("*")
     .eq("slug", slug)
     .single();
 
+  console.log(blog);
+  console.log(error);
+
   if (error || !blog) {
     return <p>Blog not found</p>;
   }
 
-  const { data: relatedBlogs } = await supabaseServer
+  const { data: relatedBlogs } = await supabase
     .from("blogs")
     .select("*")
     .neq("id", blog.id)
