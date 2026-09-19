7fdc738919cb41dac600c57635724ba5361d2241
rzkyfdlla22 <rizkyfadilla70@gmail.com>
2026-09-19 08:29:24 +0700

    Update server.ts

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,28 +1,28 @@
-1import { createServerClient } from "@supabase/ssr";
-2import { cookies } from "next/headers";
-3
-4const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-5const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
-6
-7export const createClient = (cookieStore: Awaited<ReturnType<typeof cookies>>) => {
-8  return createServerClient(
-9    supabaseUrl!,
-10    supabaseKey!,
-11    {
-12      cookies: {
-13        getAll() {
-14          return cookieStore.getAll()
-15        },
-16        setAll(cookiesToSet) {
-17          try {
-18            cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
-19          } catch {
-20            // The `setAll` method was called from a Server Component.
-21            // This can be ignored if you have middleware refreshing
-22            // user sessions.
-23          }
-24        },
-25      },
-26    },
-27  );
-28};
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+export const createClient = (cookieStore: Awaited<ReturnType<typeof cookies>>) => {
+  return createServerClient(
+    supabaseUrl!,
+    supabaseKey!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    },
+  );
+};
